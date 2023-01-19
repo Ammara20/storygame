f9f316af2ef49f9514715042c33e5d3f14038f37
dci-student <ammara.samman@dci.education>
2023-01-19 17:04:36 +0100

    first commit

---
+++ storygame.py
@@ -0,0 +1,29 @@
+answer = input("do you like camping?")
+
+if answer == "yes":
+    
+    print("wow its awesome")
+    print()
+    answer = input("do you want to go K2 or Nangaparbat?[K2/nanghaparbat]")
+
+    if answer == "K2":
+        print("there you can stuck because of bad weather")
+        print()
+        answer = input("can you live without food and cold weather?[food/cold]")
+        
+        if answer =="food":
+            print("its impossible,you will die")
+          
+        elif answer == "cold" :
+            print("are you insane,its impossible")
+
+        else:
+            print("error.you can stay home")   
+
+    elif answer == "nanghaparbat": 
+          print("Because of bad weather,NO permission!")
+
+    else:
+          print("error.you can stay home")   
+else:
+    print("you than going to paradise")
